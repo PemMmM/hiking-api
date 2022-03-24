195735838f9d66e58236e89985a2ddeaccd8ff66
ipek <ipeksoltanov@hotmail.com>
2022-03-24 16:37:40 +0200

    login updated

--- src/actions/paths/fetchPathByPathId/queries/selectPathByPathId.js
+++ src/actions/paths/fetchPathByPathId/queries/selectPathByPathId.js
@@ -1,8 +1,6 @@
-// eslint-disable-next-line import/no-unresolved
 const { submitQuery, camelKeys, getFirst } = require("~root/lib/database");
 
 const selectPathByPathId = ({ pathId }) => submitQuery`
-# debug
 SELECT 
 *
 FROM
